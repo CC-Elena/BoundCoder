ef5e8a48bcf2982a6e756a08dc8b18e6e94e270d
CC-Elena <1319412885@qq.com>
2026-07-07 15:33:34 +0800

    feat:注册read-file-tool到默认工具注册表，方便agent-core使用

--- packages/tools/src/default-registry.ts
+++ packages/tools/src/default-registry.ts
@@ -1,6 +1,8 @@
 import { fakeTool } from "./fake-tool.js";
+import { createReadFileTool } from "./read-file-tool.js";
 import { createToolRegistry } from "./tool-registry.js";
 
 export const defaultToolRegistry = createToolRegistry([
   fakeTool,
+  createReadFileTool({ rootDir: process.cwd() }),
 ]);
